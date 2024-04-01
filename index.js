0c4a04bc2bb09d98c296bf6f9945a80ad170a216
piyushwork <piyushwork78@gmail.com>
2024-04-01 18:06:30 +0530

    setted-up chatbot for 'Hello msg'

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+const Bot = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+Bot.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+Bot.on("ready", () => {
+  console.log("Bot is ready!");
+});
+
+Bot.on("message", async (message) => {
+  if (message.body === "Hello") {
+    message.reply("Hello how are you ?");
+  }
+});
+
+Bot.initialize();
